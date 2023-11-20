1c33a779c1992fa7941d10dd702019fc1f9f7d74
ks1686 <96136962+ks1686@users.noreply.github.com>
2023-11-20 13:24:49 -0500

    Clean slate

--- assets/js/toNotif.js
+++
@@ -1,9 +0,0 @@
-function toNotif(targetElement) {
-  //Get the user ID from the URL
-  var queryString = window.location.search;
-  var urlParams = new URLSearchParams(queryString);
-  var userID = urlParams.get("user_id");
-
-  //Redirect to notification management dashboard
-  window.location.href = `http://localhost:3000/notifications?user_id=${userID}`;
-}
